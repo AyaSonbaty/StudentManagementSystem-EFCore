71456f19b42988479707fcd7d31f41254f9b800a
AyaSonbaty <y1o2yo345@gmail.com>
2026-08-15 22:40:11 +0300

    Add console output to test seeded data

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace StudentManagementSystem
+﻿using StudentManagementSystem.Data;
+
+namespace StudentManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var context = new AppDbContext();
+
+            Console.WriteLine("Students:");
+            foreach (var student in context.Students)
+            {
+                Console.WriteLine($"{student.Id} - {student.FullName} - {student.Email} - Age: {student.Age} - {student.Percentage}%");
+            }
+
+            Console.WriteLine("\nCourses:");
+            foreach (var course in context.Courses)
+            {
+                Console.WriteLine($"{course.Id} - {course.Name} - {course.DurationInHours} hours");
+            }
         }
     }
 }
